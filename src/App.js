0cf6cb640e8c7f44f556aed3b84191aec0f26551
Martin J Dixon <martinjdixon@Martins-MacBook-Pro.local>
2021-01-24 11:32:14 +0000

    l

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+
 
 function App() {
+  const [modalState, setModalState] = useState(false)
+
+  const toggleModalState = () => {
+    setModalState(!modalState)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`modalBackground modalShowing-${modalState}`}>
+        <div className="modalInner">
+          <div className="modalImage">
+          <img
+              src="https://images.unsplash.com/photo-1611166983448-55084c2e63b2?ixid=MXwx
+              MjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1
+              &auto=format&fit=crop&w=634&q=80" alt="photo"
+            />
+          </div>
+          <div className="modalText">
+            <h2>Studio Portraits</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+              harum qui perferendis error molestiae ab in expedita? Quaerat
+              molestias adipisci maiores quam incidunt inventore, minima esse
+              ratione exercitationem. Porro, illo?
+            </p>
+            <form action="">
+              <button>Join now!</button>
+            </form>
+            <button className="exitButton" onClick={() => toggleModalState()}>
+              Exit Pop-Up
+            </button>
+          </div>
+        </div>
+      </div>
+      <button onClick={() => toggleModalState()}>Open Modal</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
